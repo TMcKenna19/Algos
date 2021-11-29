d28874b29e27ea431f70fc824fdf60c65b1d8ebf
tmckenna19 <tdlmckenna@gmail.com>
2021-11-29 10:19:45 -0600

    Algos

---
+++ alwaysHungry.js
@@ -0,0 +1,10 @@
+//create a function that accepts an array
+//print "yummy" each time one of the values is equal to "food"
+//if no array values are "food" then print "i'm hungry" one time
+
+//example: [1,4,"food", "hello", "cheese", "food"]
+//output:
+//yummy
+//yummy
+
+function alwaysHugry()
