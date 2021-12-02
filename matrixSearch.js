a112a1ada62b0b2a38c2c04f3edc591ad26c440c
tmckenna19 <tdlmckenna@gmail.com>
2021-12-02 10:11:13 -0600

    Algos

---
+++ matrixSearch.js
@@ -0,0 +1,34 @@
+// Matrix Search
+// Given a 2-dimensional array and a smaller 2-dimensional array, return the location of the first match found, or [-1,-1] if no match is found
+// Ex: given: 
+// [ [12,1,4,19],
+//   [3,4,11,17],
+//   [18,72,2,10],
+//   [9,15,32,16]]
+// and 
+// [ [11,17]
+//   [2,10]]
+// Return [1,2] (this is the location where the inner matrix begins)
+
+// setup:
+
+function matrixSearch(matrix, search){
+    let position = [];
+    for (let i = 0; i < matrix.length; i++){
+        for (let j = 0; j < matrix[i].length; j++){
+            if (matrix[j][i]==search[0][0]){
+                if (matrix[j][i+1]==search[0][1]){
+                    if(matrix[j+1][i]==search[1][0]){
+                        if(matrix[j+1][i+1]==search[1][1]){
+                            position = [j, i]
+                            return position
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return [-1,-1]
+}
+console.log(matrixSearch([ [12,1,4,19],[3,4,11,17],[18,72,2,10],[9,15,32,16]], [[11,17], [2,10]]))
+console.log(matrixSearch([ [12,1,4,19],[3,4,11,17],[18,72,2,10],[9,15,32,16]], [[11,17], [2,11]]))
